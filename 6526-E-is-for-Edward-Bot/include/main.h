b29541101f59abf9459a00fb669b45e3b005bec0
thwamster <thwamster@gmail.com>
2024-11-16 20:43:29 -0800

    Santa Ana Day 1 Updates + Debug Display

--- 6526-E-is-for-Edward-Bot/include/main.h
+++ 6526-E-is-for-Edward-Bot/include/main.h
@@ -3,39 +3,109 @@
 #define _PROS_MAIN_H_
 #define PROS_USE_SIMPLE_NAMES
 #define PROS_USE_LITERALS
-#include "api.h"
-#ifdef __cplusplus
 
+#include "api.h"
+
+#ifdef __cplusplus
 extern "C" {
 #endif // __cplusplus
 
-/* Competition Methods */
+/* Competition */
 void initialize();
-void disabled();
-void competition_initialize();
 void autonomous();
 void opcontrol();
 
+// Competition Helper Methods
+void updateGameInformation();
+
+// Unused Competition Methods
+void disabled();
+void competition_initialize();
+
 /* Autonomous Period Task */
-void runAutonomousInputs();
-int selectAutonomous();
-void moveForTime(std::vector<pros::MotorGroup*> motors, std::vector <int> values, int t);
+void getAutonomousInputs();
+
+// Autonomous Programs
+void runAutonomous();
+void runProgram1();
+void runProgram2();
+void runProgram3();
+void runProgram4();
+
+// Autonomous Helper Methods
+void moveForTime(std::vector<int*> targets, std::vector<int> values, int t);
+void driveForTime(int r, int l, int t);
+void intakeForTime(int i, int t);
+void scoreRing();
+void seizeGoal();
+void releaseGoal();
+void delayA(int n = 0);
 
 /* Driver Controlled Period Task */
 void getDriverInputs();
 
 /* Robot Control System Task */
 void updateRobot();
+
+// Robot Helper Methods
 int PID(int target, int sensor, double kP, double kI, double kD, int* I, int limitI, int* lastError);
 
-/* Debug Task */
+/* Screen Control System Task */
 void updateDisplay();
-void displayClear();
-void resetAllVariables();
-void on_center_button();
+void updateDisplayText();
+void updateScreenColors(pros::Color pen, pros::Color eraser);
+
+// Screen Drawing Methods
+void drawScreenBackground();
+void drawScreenButton(std::string text, int x1, int y1, int x2, int y2, pros::Color pen, pros::Color eraser);
+void drawScreenText(std::string text, int x, int y, pros::Color pen, pros::Color eraser);
+void drawScreenHeader();
+void drawScreenData();
+void drawController();
+int drawScreenButtons(std::string prompt, std::vector<std::string> icons, std::vector<pros::Color> colors);
+int waitUntilButtonPressed(int n, int width);
+
+// Screen Parsing Methods
+std::string parseCurrentPeriod();
+std::string parseCurrentSide();
+std::string parseCurrentPosition();
+std::string parseCurrentAutonomous();
+
+// Screen Helpr Methods
+int reverseScreenWidth(int n);
+int reverseScreenHeight(int n);
+bool isBetween(int n, int min, int max);
+
+/* Set Methods */
+void setCurrentPeriod(int n);
+void setCurrentSide(int n);
+void setCurrentPosition(int n);
+void setCurrentAutonomous(int n);
+void setLeftDriveTarget(int n);
+void setRightDriveTarget(int n);
+void setIntakeScoreTarget(int n);
+void setPistonTarget(int n);
+void setEnablePID(bool b);
+void setLeftDriveActual(int n);
+void setRightDriveActual(int n);
+void setDisplayTextAt(int i, std::string s);
+
+/* Get Methods */
+int getCurrentPeriod();
+int getCurrentSide();
+int getCurrentPosition();
+int getCurrentAutonomous();
+int getLeftDriveTarget();
+int getRightDriveTarget();
+int getIntakeScoreTarget();
+int getPistonTarget();
+int getLeftDriveActual();
+int getRightDriveActual();
+bool getEnablePID();
+std::string getDisplayTextAt(int i);
 
 #ifdef __cplusplus
-}
+} // extern "C"
 #endif // __cplusplus
 
 #ifdef __cplusplus
--- 6526-E-is-for-Edward-Bot/src/main.cpp
+++ 6526-E-is-for-Edward-Bot/src/main.cpp
@@ -1,4 +1,5 @@
 #include "main.h"
+
 using namespace std;
 using namespace pros;
 
@@ -7,13 +8,22 @@ using namespace pros;
 const int waitInterval = 12;
 const int maxInterval = 100;
 const int doubleInputDelay = 150;
+const int autonDelay = 300;
+
+// Screen Informaton
+const int screenWidth = 480;
+const int screenHeight = 240;
+const int screenIndentTop = 100;
+const int screenIndentRight = reverseScreenWidth(60);
+const int screenIndentBottom = reverseScreenHeight(60);
+const int screenIndentLeft = 60;
 
 // Tasks
 pros::Mutex mutex;
+pros::Task autonomousPeriod(getAutonomousInputs);
 pros::Task driverControlledPeriod(getDriverInputs);
-pros::Task autonomousPeriod(runAutonomousInputs);
 pros::Task robotControlSystem(updateRobot);
-pros::Task debug(updateDisplay);
+pros::Task screenControlSystem(updateDisplay);
 
 // Controller
 pros::Controller masterController(E_CONTROLLER_MASTER);
@@ -22,17 +32,21 @@ pros::Controller masterController(E_CONTROLLER_MASTER);
 pros::MotorGroup leftDriveMotors({-9, -10});
 pros::MotorGroup rightDriveMotors({1,2});
 pros::MotorGroup intakeMotor({7});
-pros::MotorGroup scoreMotor({8});
-adi::Pneumatics clampPistons (1, true);
+pros::MotorGroup scoreMotor({8}); 
+adi::Pneumatics clampPistons(8, true);
 
-// Robot Sensors
-// TODO: Implement
+// Game Information
+int currentPeriod{};
+int currentSide{};
+int currentPosition{};
+int currentAutonomous{};
+string displayText[9]{};
 
 // Target Variables
 int leftDriveTarget{};
 int rightDriveTarget{};
 int intakeScoreTarget{};
-bool pistonTarget{false};
+int pistonTarget{};
 
 // Actual Variables
 int leftDriveActual{};
@@ -41,156 +55,159 @@ int rightDriveActual{};
 // Toggles
 bool enablePID{false};
 
-// Debug
-string displayText[10]{};
-
-/* Competition Methods */
-
-/* 
- * Initialization method. Called by the competition.
- */
+/* Competition */
 void initialize() {
 
-	// Initialize LCD
-	lcd::initialize();
-	lcd::register_btn1_cb(on_center_button);
-
-	resetAllVariables(); // Resets variables
-	displayClear(); // Clears display
-
 	// Update tasks
-	driverControlledPeriod.suspend();
 	autonomousPeriod.suspend();
+	driverControlledPeriod.suspend();
 	robotControlSystem.suspend();
-	debug.resume();
+	screenControlSystem.resume();
+
+	updateGameInformation(); // Update game information
+
+	setCurrentPeriod(0); // Update period
 }
 
-/* 
- * Disabled state. Called by the competition. Unused.
- */
-void disabled() {}	
-
-/* 
- * Competition initialization method. Called by the competition. Unused.
- */
-void competition_initialize() {}
-
-/* 
- * Autonomous control method. Called by the competition.
- */
 void autonomous() {
-
-	resetAllVariables(); // Resets variables
-	displayClear(); // Clears display
 	
 	// Update tasks
-	driverControlledPeriod.suspend();
 	autonomousPeriod.resume();
+	driverControlledPeriod.suspend();
 	robotControlSystem.resume();
+
+	setCurrentPeriod(1);
 
 	// Control loop
 	while (true) {
 		
-		// Update display
-		displayText[2] = "Under autonomous control.";
-
-		// Wait
-		delay(waitInterval);
+		delay(waitInterval); // Wait
 	}
 }
 
-/* 
- * Operator control method. Called by the competition.
- */
 void opcontrol() {
 
-	resetAllVariables(); // Resets variables
-	displayClear(); // Clears display
-
 	// Update Tasks
-	driverControlledPeriod.resume();
 	autonomousPeriod.suspend();
+	driverControlledPeriod.resume();
 	robotControlSystem.resume();
+
+	setCurrentPeriod(2);
 
 	// Control loop
 	while (true) {
 		
-		// Update display
-		displayText[3] = "Under operator control.";
-
 		delay(waitInterval); // Wait
 	}
 }
+
+// Competition Helper Methods
+void updateGameInformation() {
+	drawScreenBackground();
+	delay(250);
+	setCurrentSide(drawScreenButtons("Select Side:", {"Red", "Blue"}, {Color::red, Color::blue}));
+	delay(250);
+	setCurrentPosition(drawScreenButtons("Select Position:", {"Positive", "Negative"}, {Color::yellow, Color::purple}));
+	delay(250);
+	setCurrentAutonomous(drawScreenButtons("Select Autonomous:", {"Program #1", "Program #2", "Program #3", "Program #4"}, {Color::red, Color::yellow, Color::green, Color::blue}));
+	delay(250);
+	drawScreenBackground();
+}
+
+// Unused Competition Methods
+void disabled() {}	
+
+void competition_initialize() {}
 
 /* Autonomous Period Task */
+void getAutonomousInputs() {
+	bool b{true};
 
-/*
- * Autonomous method. Runs the selected autonomous program, and called by the task.
- */
-void runAutonomousInputs() {
-
-	// Runs autonomous program
-	switch (selectAutonomous()) {
-
-		// Program #1: 
-		case 1:
-			// moveForTime(vector<MotorGroup*>(&leftDriveMotors, &rightDriveMotors), vector<int>{127, 127}, 100);
-			break;
-			
-		// Program #2:
-		case 2:
-			break;
+	// Control loop
+	while (true) {
 		
-		// Program #3:
-		case 3:
-			break;
-	}
-
-	resetAllVariables(); // Resets
-
-	// Update tasks
-	autonomousPeriod.suspend();
-}
-
-/*
- * Autonomous method. Selects the correct autonomous program.
- */
-int selectAutonomous() {
-	int n{};
-
-	// TODO: Implement selector
-	n = 1;
-
-	return n; // Returns
-}
-
-/*
- * Autonomous method. Moves a list of motors to specificed values for a particular interval of time.
- */
-void moveForTime(std::vector<pros::MotorGroup*> motors, std::vector<int> values, int t) {
-
-	// Time loop
-	for (int s = 0; s < t; s += waitInterval) {
-
-		// Moves all motors
-		for (int i = 0; i < sizeof(motors); i++) {
-			(*motors[i]).move(values[i]);
+		// Autonomous loop
+		if (b) {
+			
+			// Select correct autonomous program
+			runAutonomous();
+			b = false;
 		}
 
-		delay(waitInterval); // Wait
-	}
-
-	// Resets all motors
-	for (int i = 0; i < sizeof(motors); i++) {
-		(*motors[i]).move(0);
+		delay(waitInterval); // Waits
 	}
 }
 
-/* Driver Controlled Period Task */
+// Autonomous Programs
+void runAutonomous() {
+	switch (1) {
+		case 0:  // Do Nothing
+			delayA();
+			break;
 
-/* 
- * Receives inputs from controllers and sensors and updates global variables.
- */
+		case 1: // Program #1: Preload Score
+			runProgram1();
+			break;
+		
+		case 2: // Program #2:
+			runProgram2();
+			break;
+		
+		case 3: // Program #3:
+			runProgram3();
+			break;
+
+		case 4: // Program #4:
+			runProgram4();
+			break;
+		
+	}
+}
+
+void runProgram1() {
+	delayA(1000);
+	driveForTime(-127, -127, 320);
+	delayA(1000);
+	seizeGoal();
+	delayA(1000);
+	scoreRing();
+	delayA(1000);
+	driveForTime(-80, -80, 1000);
+	delayA(1000);
+	driveForTime(-20, -20, 2000);
+	delayA(3000);
+}
+
+void runProgram2() {}
+
+void runProgram3() {}
+
+void runProgram4() {}
+
+// Autonomous Helper Methods
+void moveForTime(std::vector<int*> targets, std::vector<int> values, int t) {
+
+	// Updates all values
+	for (int i = 0; i < targets.size() && i < values.size(); i++) {
+		*targets[i] = values[i];
+	}
+
+	delay(t); // Wait
+
+	// Resets all values
+	for (int i = 0; i < targets.size() && i < values.size(); i++) {
+		*targets[i] = 0;
+	}
+}
+
+void driveForTime(int r, int l, int t) { moveForTime({&rightDriveTarget, &leftDriveTarget}, {r, l}, t); }
+void intakeForTime(int i, int t) { moveForTime({&intakeScoreTarget}, {i}, t); }
+void scoreRing() { intakeForTime(1, 2500); }
+void seizeGoal() { setPistonTarget(1); }
+void releaseGoal() { setPistonTarget(0); }
+void delayA(int n) { (n < autonDelay) ? delay(autonDelay) : delay(n); }
+
+/* Driver Controlled Period Task */
 void getDriverInputs() {
 
 	// Raw controller values
@@ -246,47 +263,43 @@ void getDriverInputs() {
 		mutex.take(maxInterval); // Begin Exclusivity
 
 		// Sets Targets
-		leftDriveTarget = axis3;
-		rightDriveTarget = axis2;
+		setLeftDriveTarget(axis3);
+		setRightDriveTarget(axis2);
 
 		if (buttonA == 1 && aDelayCount >= doubleInputDelay) {
-			enablePID = !enablePID;
+			setEnablePID(!getEnablePID);
 			aDelayCount = 0;
 		}
 		aDelayCount++;
 		
 		if (buttonUp == 1 && buttonDown == 0) {
-			pistonTarget = true;
+			setPistonTarget(1);
 		}
 		else if (buttonUp == 0 && buttonDown == 1){
-			pistonTarget = false;
+			setPistonTarget(0);
 		}
 
 		if (buttonR1 == 1 && buttonR2 == 0) {
-			intakeScoreTarget = 1;
+			setIntakeScoreTarget(1);
 		}
 		else if (buttonR1 == 0 && buttonR2 == 1) {
-			intakeScoreTarget = -1;
+			setIntakeScoreTarget(-1);
 		}
 		else {
-			intakeScoreTarget = 0;
+			setIntakeScoreTarget(0);
 		}
 
-		mutex.give(); // End Exclusivity
-
 		// Sets actuals
-		rightDriveActual = velocityR;
-		leftDriveActual = velocityL;
+		setRightDriveActual(velocityR);
+		setLeftDriveActual(velocityL);
+
+		mutex.give(); // End Exclusivity
 
 		delay(waitInterval); // Wait
 	}
 }
 
 /* Robot Control System Task */
-
-/* 
- * Driver control system. Updates motors and electronics from target variables.
- */
 void updateRobot() {
 	int rightDriveI = 0;
 	int rightDriveLastError = 0;
@@ -296,37 +309,34 @@ void updateRobot() {
 	// Control loop
 	while (true) {
 
-		// Update display
-		displayText[4] = "Updating robot.";
-
 		mutex.take(maxInterval); // Begin Exclusivity
 
-		if (enablePID) {
-			rightDriveMotors.move(PID(rightDriveTarget, rightDriveActual, 0.6, 0.2, 0.2, &rightDriveI, 10, &rightDriveLastError));
-			leftDriveMotors.move(PID(leftDriveTarget, leftDriveActual, 0.6, 0.2, 0.2, &leftDriveI, 10, &leftDriveLastError));
+		if (getEnablePID()) {
+			rightDriveMotors.move(PID(getRightDriveTarget(), getRightDriveActual(), 0.6, 0.2, 0.2, &rightDriveI, 10, &rightDriveLastError));
+			leftDriveMotors.move(PID(getLeftDriveTarget(), getLeftDriveActual(), 0.6, 0.2, 0.2, &leftDriveI, 10, &leftDriveLastError));
 		}
 		else {
-			rightDriveMotors.move(rightDriveTarget);
-			leftDriveMotors.move(leftDriveTarget);
+			rightDriveMotors.move(getRightDriveTarget());
+			leftDriveMotors.move(getLeftDriveTarget());
 		}
 
-		if (pistonTarget) {
+		if (getPistonTarget() == 1) {
 			clampPistons.extend();
 		}
 		else {
 			clampPistons.retract();
 		}
 
-		if (intakeScoreTarget == 1) {
+		if (getIntakeScoreTarget() == 1) {
 			intakeMotor.move(90);
 			scoreMotor.move(80);
 		}
-		else if (intakeScoreTarget == 2) {
+		else if (getIntakeScoreTarget() == -1) {
 			intakeMotor.move(-90);
 			scoreMotor.move(-80);
 			
 		}
-		else if (intakeScoreTarget == 0) {
+		else if (getIntakeScoreTarget() == 0) {
 			intakeMotor.move(0);
 			scoreMotor.move(0);
 		}
@@ -337,9 +347,7 @@ void updateRobot() {
 	}
 }
 
-/* 
- * PID control system method.
- */
+// Robot Helper Methods
 int PID(int target, int sensor, double kP, double kI, double kD, int* I, int limitI, int* lastError) {
 	int PID = 0;
 	int error = target - sensor;
@@ -361,90 +369,194 @@ int PID(int target, int sensor, double kP, double kI, double kD, int* I, int lim
 	return PID; // Returns
 }
 
-/* Debug Task */
-
-/* 
- * Debug method. Updates the LCD on the brain.
- */
+/* Screen Control System Task */
 void updateDisplay() {
+	int bgDelay{};
 
-	displayClear(); // Clears display
+	fill_n(displayText, (sizeof(displayText)/sizeof(displayText[0])), "");
+	setDisplayTextAt(0, "6526 E-is-for-Edward Bot");
+	drawScreenBackground();
 
 	// Control loop
 	while (true) {
 
-		mutex.take(maxInterval); // Begin exclusivity
-	
-		// Updates lines from variables
-		displayText[5] = "Right Target: " + to_string(rightDriveTarget) + ". Right Actual: " + to_string(rightDriveActual);
-		displayText[6] = "Left Target: " + to_string(leftDriveTarget) + ". Left Actual: " + to_string(leftDriveActual);
-		displayText[7] = "Intake Target: " + to_string(intakeScoreTarget);
-		displayText[8] = "Pnemautic Target: " + to_string(pistonTarget);
-		displayText[9] = "PID enabled: " + to_string(enablePID);
-		
-		mutex.give(); // End exclusivity
+		// If during driver controlled or autonomous periods
+		if (getCurrentPeriod() == 2 || getCurrentPeriod() == 3) {
 
-		// Loop through all lines
-		for (int i = 0; i < sizeof(displayText); i++) {
+			// Update text to display
+			updateDisplayText();
 
-			// Update LCD
-			lcd::set_text(i, displayText[i]);
+			// Display background
+			if (bgDelay >= 100) {
+				drawScreenBackground();
+				bgDelay = 0;
+			}
+			bgDelay++;
+			
+			// Display foregrounds
+			drawScreenHeader();
+			drawScreenData();
+			drawController();
 		}
 
 		delay(waitInterval); // Wait
 	}
 }
 
-/*
- * Debug method. Clears the array of strings that displays on the screen.
- */
-void displayClear() {
+void updateDisplayText() {
 
-	// Clears display
-	fill_n(displayText, sizeof(displayText), "");
-
-	// Assigns initialization text
-	displayText[1] = "~~~ 6526-E-is-for-Edward-Bot ~~~";
-}
-
-/*
- * Reset method. Resets all instance variables.
- */
-void resetAllVariables() {
-
-	// Resets targets
-	leftDriveTarget = 0;
-	rightDriveTarget = 0;
-	intakeScoreTarget = 0;
-	pistonTarget = false;
-
-	// Resets actuals
-	leftDriveActual = 0;
-	rightDriveActual = 0;
+	mutex.take(maxInterval); // Begin exclusivity
 	
-	// Resets toggles
-	enablePID = false;
+	// Updates lines from variables
+	setDisplayTextAt(1, "Period: " + parseCurrentPeriod());
+	setDisplayTextAt(2, "Side: " + parseCurrentSide() + ". Start Position: " + parseCurrentPeriod());
+	setDisplayTextAt(3, "Selected Autonomous: " + parseCurrentAutonomous());
+	setDisplayTextAt(4, "Right Target: " + to_string(rightDriveTarget) + ". Right Actual: " + to_string(rightDriveActual));
+	setDisplayTextAt(5, "Left Target: " + to_string(leftDriveTarget) + ". Left Actual: " + to_string(leftDriveActual));
+	setDisplayTextAt(6, "Intake Target: " + to_string(intakeScoreTarget) + ". Piston Target: " + to_string(pistonTarget));
+	setDisplayTextAt(7, "PID enabled: " + to_string(enablePID));
+		
+	mutex.give(); // End exclusivity
 }
 
-/* 
- * Center Button reaction method. Unused.
- */
-void on_center_button() {}
+void updateScreenColors(pros::Color pen, pros::Color eraser) {
+	pros::screen::set_pen(pen);
+	pros::screen::set_eraser(eraser);
+}
 
-/* 
- * Electronics
- *
- * Drivetrain: 
- * 	  Left front: Port 10
- * 	  Left back: Port 9
- * 	  Right front: Port 2
- * 	  Right back: Port 1
- * Intake-Score:
- * 	  Intake: Port 7 
- * 	  Score: Port 8
- * Inertial Sensors:
- * 	  Left side: Port 2
- * 	  Right side: Port 1
- * Radio: Port 21
- * Clamp Pistons: 3 Wire Ouput A
- */
+// Screen Drawing Methods
+void drawScreenBackground() {
+	updateScreenColors(Color::white, Color::dark_green);
+	pros::screen::erase_rect(0, 0, 480, 240);
+}
+
+void drawScreenButton(std::string text, int x1, int y1, int x2, int y2, pros::Color pen, pros::Color eraser) {
+	updateScreenColors(pen, eraser);
+	pros::screen::erase_rect(x1, y1, x2, y2);
+	drawScreenText(text, (x1 + 20), (y1 + 20), pen, eraser);
+}
+
+void drawScreenText(std::string text, int x, int y, pros::Color pen, pros::Color eraser) {
+	updateScreenColors(pen, eraser);
+	pros::screen::print(E_TEXT_LARGE, x, y, text.c_str());
+}
+
+void drawScreenHeader() {
+	updateScreenColors(Color::gold, Color::dark_green);
+	pros::screen::print(pros::E_TEXT_LARGE_CENTER, 1, 10, getDisplayTextAt(0).c_str());
+}
+
+void drawScreenData() {
+	updateScreenColors(Color::white, Color::dark_green);
+	for (int i = 1; i < (sizeof(displayText)/sizeof(displayText[0])); i++) {
+		pros::screen::print(pros::E_TEXT_MEDIUM, 5, ((i + 1) * 22), getDisplayTextAt(i).c_str());
+	}
+}
+
+void drawController() {
+	masterController.print(0, 0, ("6526-E: " + parseCurrentSide()).c_str());
+}
+
+int drawScreenButtons(std::string prompt, std::vector<std::string> icons, std::vector<pros::Color> colors) {
+	int n = std::min(icons.size(), colors.size());
+	int width = (screenIndentRight - screenIndentLeft) / n;
+
+	drawScreenBackground();
+	drawScreenText(prompt, 60, 60, Color::white, Color::dark_green);
+	for (int i = 0; i < n; i++) {
+		drawScreenButton(icons[i], (screenIndentLeft + (width * i)), screenIndentTop, (screenIndentLeft + (width * (i + 1))), screenIndentBottom, Color::white, colors[i]);
+	}
+	drawScreenHeader();
+
+	return waitUntilButtonPressed(n, width);
+}
+
+int waitUntilButtonPressed(int n, int width) {
+	while (pros::screen::touch_status().touch_status != pros::E_TOUCH_HELD) {
+		delay(waitInterval);
+	}
+
+	int x = pros::screen::touch_status().x;
+	int y = pros::screen::touch_status().y;
+
+	if (!isBetween(y, screenIndentTop, screenIndentBottom)) {
+		return waitUntilButtonPressed(n, width);
+	}
+
+	for (int i = 0; i < n; i++) {
+		if (isBetween(x, (screenIndentLeft + (width * i)), (screenIndentLeft + (width * (i + 1))))) {
+			return (i + 1);
+		}
+	}
+
+	return waitUntilButtonPressed(n, width);
+}
+
+// Screen Parsing Methods
+std::string parseCurrentPeriod() {
+	switch (getCurrentPeriod()) {
+		case 1: return "Initializing";
+		case 2: return "Driver Controlled";
+		case 3: return "Autonomous Controlled";
+		default: return "Unknown";
+	}
+}
+
+std::string parseCurrentSide() {
+	switch (getCurrentSide()) {
+		case 1: return "Red";
+		case 2: return "Blue";
+		default: return "Unknown";
+	}
+}
+
+std::string parseCurrentPosition() {
+	switch (getCurrentPosition()) {
+		case 1: return "Positive";
+		case 2: return "Negative";
+		default: return "Unknown";
+	}
+}
+
+std::string parseCurrentAutonomous() {
+	switch (getCurrentAutonomous()) {
+		case 1: return "Program #1: Preload Score";
+		case 2: return "Program #2";
+		case 3: return "Program #3";
+		case 4: return "Program #4";
+		default: return "Unknown";
+	}
+}
+
+// Screen Helpr Methods
+int reverseScreenWidth(int n) { return screenWidth - n; }
+int reverseScreenHeight(int n) { return screenHeight - n; }
+bool isBetween(int n, int min, int max) { return n >= min && n <= max; }
+
+/* Set Methods */
+void setCurrentPeriod(int n) { currentPeriod = n; }
+void setCurrentSide(int n) { currentSide = n; }
+void setCurrentPosition(int n) { currentPosition = n; }
+void setCurrentAutonomous(int n) { currentAutonomous = n; }
+void setLeftDriveTarget(int n) { leftDriveTarget = n; }
+void setRightDriveTarget(int n) { rightDriveTarget = n; }
+void setIntakeScoreTarget(int n) { intakeScoreTarget = n; }
+void setPistonTarget(int n) { pistonTarget = n; }
+void setEnablePID(bool b) { enablePID = b; }
+void setLeftDriveActual(int n) { leftDriveActual = n; }
+void setRightDriveActual(int n) { rightDriveActual = n; }
+void setDisplayTextAt(int i, std::string s) { displayText[i] = s; }
+
+/* Get Methods */
+int getCurrentPeriod() { return currentPeriod; }
+int getCurrentSide() { return currentSide; }
+int getCurrentPosition() { return currentPosition; }
+int getCurrentAutonomous() { return currentAutonomous; }
+int getLeftDriveTarget() { return leftDriveTarget; }
+int getRightDriveTarget() { return rightDriveTarget; }
+int getIntakeScoreTarget() { return intakeScoreTarget; }
+int getPistonTarget() { return pistonTarget; }
+int getLeftDriveActual() { return leftDriveActual; }
+int getRightDriveActual() { return rightDriveActual; }
+bool getEnablePID() { return enablePID; }
+std::string getDisplayTextAt(int i) { return displayText[i]; }
